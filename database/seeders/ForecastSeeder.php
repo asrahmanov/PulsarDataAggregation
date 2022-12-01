40077be3baff3d099c098c875f64e298f9c34e91
asrahmanov <asrahmanvo@gmail.com>
2022-12-01 19:44:20 +0300

    Форма по прогнозу

--- database/seeders/ForecastSeeder.php
+++ database/seeders/ForecastSeeder.php
@@ -163,7 +163,8 @@ class ForecastSeeder extends Seeder
 
 
 //        $filename_2021 = storage_path('app/forecast/36/2021/Форма по прогнозу.xlsx');
-        $filename_2022 = storage_path('app/forecast/36/2022/forecast.xlsx');
+//        $filename_2022 = storage_path('app/forecast/36/2022/forecast.xlsx');
+        $filename_2022 = storage_path('app/exp/36/2022/Форма по ожидаемой выручке.xlsx');
 //        $filename_2023 = storage_path('app/forecast/36/2023/Форма по прогнозу.xlsx');
 
 //        if (file_exists($filename_2021)) {
