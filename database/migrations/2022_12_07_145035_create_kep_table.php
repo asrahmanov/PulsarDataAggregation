a8ee930ffaf748b2b831d9ab8c78823ac338f8f2
asrahmanov <asrahmanvo@gmail.com>
2022-12-07 15:04:00 +0300

    Миграция

---
+++ database/migrations/2022_12_07_145035_create_kep_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeptTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kep', function (Blueprint $table) {
+            $table->id();
+            $table->string('source_name');
+            $table->string('pp');
+            $table->string('nomenclature');
+            $table->string('action');
+            $table->date('date_action');
+            $table->integer('val');
+            $table->timestamps();
+            $table->softDeletes();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kep');
+    }
+}
