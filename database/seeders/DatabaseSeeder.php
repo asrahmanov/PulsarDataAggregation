4716ce166d1869fbc3a947ca032dbe494c50b8e9
asrahmanov <asrahmanvo@gmail.com>
2022-12-07 15:33:39 +0300

    kep seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,14 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-        ContractAndFactSeeder::class,
+//        ContractAndFactSeeder::class,
 //        PlanContractSeeder::class,
-        ExpectedRevenueSeeder::class,
+//        ExpectedRevenueSeeder::class,
 //        OperationalPlanSeeder::class,
 //        BudgetSeeder::class,
 //        DefectSeeder::class,
 //          TestSeeder::class,
-//        ForecastSeeder::class
+//        ForecastSeeder::class,
+            KepSeeder::class
         ]);
     }
 }
