f48a33268b613a25034e0e02b432b5d3495dae61
asrahmanov <asrahmanvo@gmail.com>
2022-12-14 10:51:58 +0300

    Загрузка плана

--- database/migrations/2022_12_07_145035_create_kep_plan_table.php
+++ database/migrations/2022_12_07_145035_create_kep_plan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKepTable extends Migration
+class CreateKepPlanTable extends Migration
 {
     /**
      * Run the migrations.
