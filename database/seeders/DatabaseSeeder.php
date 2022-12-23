3285700e6d9725c4bff60b67d7176a8fe6fe718c
asrahmanov <asrahmanvo@gmail.com>
2022-12-23 10:01:25 +0300

    Загрзука файлов от 22_12_2022

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-//        ContractAndFactSeeder::class,
+        ContractAndFactSeeder::class,
 //        PlanContractSeeder::class,
         ExpectedRevenueSeeder::class,
-//        OperationalPlanSeeder::class,
+        OperationalPlanSeeder::class,
 //        BudgetSeeder::class,
 //        DefectSeeder::class,
 //          TestSeeder::class,
