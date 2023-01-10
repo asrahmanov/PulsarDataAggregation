5f839778ee32e8fa0ba15ec93d7a2c35234eeaca
asrahmanov <asrahmanvo@gmail.com>
2023-01-10 19:13:08 +0300

    Создание миграций

---
+++ database/migrations/2023_01_10_160907_create_table_financial_indicators_net_profit.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableFinancialIndicatorsNetProfit extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_financial_indicators_net_profit', function (Blueprint $table) {
+            $table->id();
+            $table->string('mount');
+            $table->string('year');
+            $table->DOUBLE('budget');
+            $table->DOUBLE('fact');
+            $table->DOUBLE('forecast');
+            $table->DOUBLE('last_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_financial_indicators_net_profit');
+    }
+}
